2f8c85249cfbbd8bd98d6102ae2ecfa4c4f5b3ee
kruti-saliya <krutisaliya@weetechsolution.com>
2025-02-28 15:46:14 +0530

    Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button>Hey</Button>;
+  return (
+    <div className="mt-40">
+      <h1>Hello, Next.js!</h1>
+      <Button>Click me</Button>
+      <p>This is a simple example of a Next.js page.</p>
+    </div>
+  );
 }
