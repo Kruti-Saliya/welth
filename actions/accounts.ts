2d387f3a5008ecbee67e1521a15bf1f92c54b1ba
kruti-saliya <krutisaliya@weetechsolution.com>
2025-03-03 15:18:58 +0530

    Chnage default account

---
+++ actions/accounts.ts
@@ -0,0 +1,66 @@
+"use server"
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { Account } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+import { revalidatePath } from "next/cache";
+
+export interface ISerializedAccount extends Omit<Account, "balance"> {
+  balance: number;
+  amount?: number;
+}
+
+const serializeTransaction = (account: Account): ISerializedAccount => {
+  const serializedAccount: ISerializedAccount = {
+    ...account,
+    balance:
+      account.balance instanceof Decimal
+        ? account.balance.toNumber()
+        : Number(account.balance),
+  };
+
+  if ("amount" in account && account.amount instanceof Decimal) {
+    serializedAccount.amount = account.amount.toNumber();
+  }
+
+  return serializedAccount;
+};
+
+export async function updateDefaultAccount(accountId:string) {
+    try {
+      const { userId } = await auth();
+      if (!userId) throw new Error("Unauthorized");
+  
+      const user = await db.user.findUnique({
+        where: { clerkUserId: userId },
+      });
+  
+      if (!user) {
+        throw new Error("User not found");
+      }
+  
+      await db.account.updateMany({
+        where: {
+          userId: user.id,
+          isDefault: true,
+        },
+        data: { isDefault: false },
+      });
+  
+      const account = await db.account.update({
+        where: {
+          id: accountId,
+          userId: user.id,
+        },
+        data: { isDefault: true },
+      });
+  
+      revalidatePath("/dashboard");
+      return { success: true, data: serializeTransaction(account) };
+    } catch (error) {
+        return { 
+          success: false, 
+          error: error instanceof Error ? error.message : "An unknown error occurred" 
+        };
+      }
+  }
--- app/(main)/dashboard/_components/AccountCard.tsx
+++ app/(main)/dashboard/_components/AccountCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { ISerializedAccount } from "@/actions/dashboard";
 import {
   Card,
@@ -10,6 +11,9 @@ import {
 import { Switch } from "@/components/ui/switch";
 import { ArrowDownRight, ArrowUpRight } from "lucide-react";
 import Link from "next/link";
+import { toast } from "sonner";
+import useFetch from "@/hooks/use-fetch";
+import { updateDefaultAccount } from "@/actions/accounts";
 
 interface AccountCardProps {
   account: ISerializedAccount;
@@ -17,6 +21,37 @@ interface AccountCardProps {
 
 const AccountCard: React.FC<AccountCardProps> = ({ account }) => {
   const { name, type, balance, id, isDefault } = account;
+
+  const {
+    loading: updateDefaultLoading,
+    fn: updateDefaultFn,
+    data: updatedAccount,
+    error,
+  } = useFetch(updateDefaultAccount);
+
+  const handleDefaultChange = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault(); // Prevent navigation
+
+    if (isDefault) {
+      toast.warning("You need atleast 1 default account");
+      return; // Don't allow toggling off the default account
+    }
+
+    await updateDefaultFn(id);
+  };
+
+  useEffect(() => {
+    if (updatedAccount?.success) {
+      toast.success("Default account updated successfully");
+    }
+  }, [updatedAccount]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "Failed to update default account");
+    }
+  }, [error]);
+
   return (
     <Card className="hover:shadow-md transition-shadow group relative">
       <Link href={`/account/${id}`}>
@@ -26,6 +61,9 @@ const AccountCard: React.FC<AccountCardProps> = ({ account }) => {
           </CardTitle>
           <Switch
             checked={isDefault}
+            onClick={handleDefaultChange}
+            disabled={updateDefaultLoading}
+            className="cursor-pointer"
           />
         </CardHeader>
         <CardContent>
