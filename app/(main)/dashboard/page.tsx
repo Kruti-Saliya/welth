ce39a87ff9c3c210adc6a0a3b9fa1932d5ecdbbc
kruti-saliya <krutisaliya@weetechsolution.com>
2025-04-04 18:12:35 +0530

    Dashboard expense Pie Chart

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,11 +1,13 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
+import { Transaction } from "@/types"; 
 import CreateAccountDrawer from "@/components/CreateAccountDrawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { Suspense } from "react";
 import AccountCard from "./_components/AccountCard";
 import { getCurrentBudget } from "@/actions/budget";
 import { BudgetProgress } from "./_components/BudgetProgress";
+import { DashboardOverview } from "./_components/transaction-overview";
 
 const DashboardPage = async () => {
   const accounts = await getUserAccounts();
@@ -16,13 +18,26 @@ const DashboardPage = async () => {
   if (defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
+  const rawTransactions = (await getDashboardData()) as Awaited<ReturnType<typeof getDashboardData>>;
+  const transactions = rawTransactions.map((transaction: Transaction) => ({
+    date: transaction.date,
+    category: transaction.category,
+    ...transaction,
+  }));
   return (
     <div className="space-y-8">
+      {defaultAccount && (
         <BudgetProgress
           initialBudget={budgetData?.budget ?? null}
           currentExpenses={budgetData?.currentExpenses || 0}
         />
-
+      )}
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+        />
+      </Suspense>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
